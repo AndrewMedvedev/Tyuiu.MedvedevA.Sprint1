9869e783909f1ce05a406d0b0adb6e13029ff0a6
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-09-21 22:16:31 +0500

    Task2

---
+++ Tyuiu.MedvedevA.Sprint1.Task2.V15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint1.Task2.V15.Lib;
+namespace Tyuiu.MedvedevA.Sprint1.Task2.V15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Квадрат числа X = " + ds.CalculateCubeVolume(x));
+
+            Console.ReadKey();
+        }
+    }
+}
+
