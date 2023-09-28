baac0be37c01c3db8499569f09b02bcb466ae801
Андрей Медведев <Tyuiu.MedvedevA@outlook.com>
2023-09-28 22:00:10 +0500

    Task6

---
+++ Tyuiu.MedvedevA.Sprint1.Task6.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MedvedevA.Sprint1.Task6.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MedvedevA.Sprint1.Task6.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f98e954-4c94-47ab-8b25-46172579c592")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.MedvedevA.Sprint1.Task6.V1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MedvedevA.Sprint1.Task6.V1.Lib;
+
+namespace Tyuiu.MedvedevA.Sprint1.Task6.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string x;
+            Console.WriteLine("Введите символ и нажмите <ENTER>");
+            Console.WriteLine("Для завершения нажмите точку");
+            x = Console.ReadLine();
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Символ: " + x + " Код: " + ds.SymbolCode(x));
+            {
+                ConsoleKeyInfo cons = Console.ReadKey();
+                while (cons.Key != ConsoleKey.Escape)
+                {
+                    cons = Console.ReadKey();
+                }
+            }
+        }
+    }
+}
